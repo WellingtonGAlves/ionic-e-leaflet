a7c3413244d2f909239d74e4115c4b22f46eee39
Wellington <wellington_999@live.com>
2022-05-14 10:12:06 -0300

    marker, circle and click on map ok

--- src/app/leaflet/mapa/mapa.page.ts
+++ src/app/leaflet/mapa/mapa.page.ts
@@ -11,11 +11,16 @@ export class MapaPage implements OnInit, AfterViewInit {
   map: any;
   showMap: boolean;
   marker: any;
+  circle: any;
+  popup: any;
 
   constructor() { }
 
   ngOnInit() {
-
+    setTimeout(() => {
+      this.initMap();
+      // alert('Loading...');
+    },1000);
   }
 
   ngAfterViewInit(): void {
@@ -31,10 +36,35 @@ export class MapaPage implements OnInit, AfterViewInit {
       });
 
       tiles.addTo(this.map);
-
-      this.marker = L.marker([51.5, -0.09]).addTo(this.map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      this.addMarker();
+      //add circle
+      this.addCircle();
+      //call function after click on mapa
+      this.map.on('click', (e: any) => {
+        this.onMapClick(e,this.map);
+      });
     }
   }
+  addMarker(){
+    //add marker
+    this.marker = L.marker([51.5, -0.09]).addTo(this.map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.',{closeButton: false})
+    .openPopup();
+  }
+  addCircle(){
+    this.circle = L.circle([51.508, -0.11], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(this.map);
+  }
+  onMapClick(e,map) {
+    // alert("You clicked the map at " + e.latlng);
+    const popup = L.popup();
+    popup
+        .setLatLng(e.latlng)
+        .setContent('You clicked the map at ' + e.latlng.toString())
+        .openOn(map);
+}
 }
